93a4f3774dc09bd7be99b84eacf3e54cabd5cf12
Tl Yim <tlyim@users.noreply.github.com>
2025-02-24 04:59:20 +0000

    Update SetupRunOnce_Lightning.ai.sh

--- SetupRunOnce_Lightning.ai.sh
+++ SetupRunOnce_Lightning.ai.sh
@@ -31,18 +31,18 @@
 sudo apt update -qq
 
 # install two helper packages we need
-sudo apt install --no-install-recommends software-properties-common dirmngr
+sudo apt install -y --no-install-recommends software-properties-common dirmngr
 
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+#wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
 # add the repo from CRAN -- lsb_release adjusts to 'noble' or 'jammy' or ... as needed
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+#sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 # install R itself
-sudo apt install -y --no-install-recommends r-base
+#sudo apt install -y --no-install-recommends r-base
 
 sudo apt-get update
 # Install Ubuntu packages harfbuzz, libfribidi, etc; then find the installed locations
@@ -70,7 +70,7 @@ source ~/.bashrc
 
 # Upgrade pip and install radian
 python3 -m pip install --upgrade pip
-pip3 install -U radian
+#pip3 install -U radian
 
 # Add the following lines to the end of ~/.bashrc  
 echo '
