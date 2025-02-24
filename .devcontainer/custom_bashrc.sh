051443857cdb56609fa6f5582423158be525df0d
Tl Yim <tlyim@users.noreply.github.com>
2025-02-24 20:03:27 +0000

    also installed nano

--- .devcontainer/custom_bashrc.sh
+++ .devcontainer/custom_bashrc.sh
@@ -8,7 +8,7 @@ __bash_prompt() {
     local gitbranch='`\
         if [ "$(git config --get devcontainers-theme.hide-status 2>/dev/null)" != 1 ] && [ "$(git config --get codespaces-theme.hide-status 2>/dev/null)" != 1 ]; then \
             export BRANCH="$(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null)"; \
-            if [ "${BRANCH:-}" != ""; then \
+            if [ "${BRANCH:-}" != "" ]; then \
                 echo -n "\[\033[0;36m\](\[\033[1;31m\]${BRANCH:-}" \
                 && if [ "$(git config --get devcontainers-theme.show-dirty 2>/dev/null)" = 1 ] && \
                     git --no-optional-locks ls-files --error-unmatch -m --directory --no-empty-directory -o --exclude-standard ":/*" > /dev/null 2>&1; then \
